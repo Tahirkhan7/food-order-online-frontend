a65620c81159e088df0bb2487d82528aca90bc0f
Tahir Khan <mahitk98@gmail.com>
2024-12-01 03:34:45 +0530

    changed base url for address api backend

--- services/address.js
+++ services/address.js
@@ -3,17 +3,17 @@ import axios from "axios";
 const BASE_URL = import.meta.env.API_URL;
 
 export const getAllAddresses = async (email) => {
-  const res = await axios.get(`${BASE_URL}/api/address/all/${email}`);
+  const res = await axios.get(`https://food-order-online-backend.onrender.com/api/address/all/${email}`);
   return res;
 };
 
 export const getAddressById = async (id) => {
-  const res = await axios.get(`${BASE_URL}/api/address/${id}`);
+  const res = await axios.get(`https://food-order-online-backend.onrender.com/api/address/${id}`);
   return res;
 };
 
 export const addAddress = async (data) => {
-  const res = await axios.post(`${BASE_URL}/api/address/add`, data, {
+  const res = await axios.post(`https://food-order-online-backend.onrender.com/api/address/add`, data, {
     headers: {
       "Content-Type": "application/json",
     },
@@ -22,7 +22,7 @@ export const addAddress = async (data) => {
 };
 
 export const updateAddress = async (data) => {
-  const res = await axios.put(`${BASE_URL}/api/address/edit`, data, {
+  const res = await axios.put(`https://food-order-online-backend.onrender.com/api/address/edit`, data, {
     headers: {
       "Content-Type": "application/json",
     },
